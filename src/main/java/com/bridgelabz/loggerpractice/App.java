0da50dfd89016f98dfbdacf3165cafda665313f0
shwetap29 <shwetaprasad48@gmail.com>
2021-05-18 10:36:26 +0530

    Added: functionality to print hello message

---
+++ src/main/java/com/bridgelabz/loggerpractice/App.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.loggerpractice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
